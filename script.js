3c67d98e60d658dd491f568e8d83a57583c2e82d
Camila Ferreira <mila.e.ferreira28@gmail.com>
2024-05-11 23:36:09 -0300

    terminado a pagina sobre

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+import Swal from 'sweetalert2'
+
 function barraFerramentas() {
     alert('Estou testando')
 }
+
+Swal.fire({
+    title: 'Erro!',
+    text: 'Quer continuar?',
+    icon: 'error',
+    confirmButtonText:'Cool'
+})
