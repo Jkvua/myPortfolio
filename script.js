08e7dfe2eed6fef502462765c6d3b665eac30751
Camila Ferreira <mila.e.ferreira28@gmail.com>
2024-05-12 22:13:08 -0300

    criando o botao

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-import Swal from 'sweetalert2'
+//import Swal from 'sweetalert2'
 
 function barraFerramentas() {
-    alert('Estou testando')
+    Swal.fire({
+        title: 'Erro!',
+        text: 'Quer continuar?',
+        icon: 'info',
+        confirmButtonText:'Ok'
+    })
 }
 
-Swal.fire({
-    title: 'Erro!',
-    text: 'Quer continuar?',
-    icon: 'error',
-    confirmButtonText:'Cool'
-})
