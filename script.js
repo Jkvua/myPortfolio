0725589a03b2459af78206658f8f4ad4a4152bdc
Camila Ferreira <mila.e.ferreira28@gmail.com>
2024-05-13 19:48:28 -0300

    termino de todas as paginas

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-//import Swal from 'sweetalert2'
-
 function barraFerramentas() {
-    Swal.fire({
-        title: 'Erro!',
-        text: 'Quer continuar?',
-        icon: 'info',
-        confirmButtonText:'Ok'
-    })
+  Swal.fire({
+    title: "Conheça-me",
+    //icon: 'info',
+    text: "Caso queira saber mais sobre mim",
+    html: '<a class="rojo" href="https://amazon.com.br"><i class="fa-solid fa-camera-retro"></i> vsco</a> <br> <a class="rojo"><i class="fa-brands fa-linkedin"></i> linkedin</a> <br> <a class="rojo"><i class="fa-solid fa-newspaper"></i> currículo</a>',
+    confirmButtonText: "Fechar",
+    cancelButtonArialLabel: "Thumbs down",
+  });
 }
-
