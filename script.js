fca7fa3f763a9715192101a97e8a0406f3c578ec
Camila Ferreira <mila.e.ferreira28@gmail.com>
2024-05-14 14:30:28 -0300

    inderecando links

--- script.js
+++ script.js
@@ -3,8 +3,26 @@ function barraFerramentas() {
     title: "Conheça-me",
     //icon: 'info',
     text: "Caso queira saber mais sobre mim",
-    html: '<a class="rojo" href="https://amazon.com.br"><i class="fa-solid fa-camera-retro"></i> vsco</a> <br> <a class="rojo"><i class="fa-brands fa-linkedin"></i> linkedin</a> <br> <a class="rojo"><i class="fa-solid fa-newspaper"></i> currículo</a>',
+    html: '<a class="rojo" href="https://vsco.co/jkvua"><i class="fa-solid fa-camera-retro"></i> vsco</a> <br> <a class="rojo" href="https://www.linkedin.com/in/camila-ferreira-de-almeida-17b130272/"><i class="fa-brands fa-linkedin"></i> linkedin</a> <br> <a class="rojo" href="https://intranet.ielsc.org.br/intranet/ferramentas/crmiel/sge/candidato/curriculo.pdf?candidato=2195763&vaga="><i class="fa-solid fa-newspaper"></i> currículo</a><br><a class="rojo" href="https://open.spotify.com/user/6v1kv7fxuz60xhwevzou86lk8?si=95ZANTW4S3C4wZ1LfMusqQ"><i class="fa-brands fa-spotify"></i> spotify</a>',
     confirmButtonText: "Fechar",
     cancelButtonArialLabel: "Thumbs down",
+  });
+}
+
+function informacoesMe() {
+  Swal.fire({
+    title: 'Conheça-me',
+    text: 'Caso queira saber mais de mim',
+    html: '<a class="rus" href="https://vsco.co/jkvua"><i class="fa-solid fa-camera-retro"></i> vsco</a> <br> <a class="rus" href="https://www.linkedin.com/in/camila-ferreira-de-almeida-17b130272/"><i class="fa-brands fa-linkedin"></i> linkedin</a> <br> <a class="rus" href="https://intranet.ielsc.org.br/intranet/ferramentas/crmiel/sge/candidato/curriculo.pdf?candidato=2195763&vaga="><i class="fa-solid fa-newspaper"></i> currículo</a><br><a class="rus" href="https://open.spotify.com/user/6v1kv7fxuz60xhwevzou86lk8?si=95ZANTW4S3C4wZ1LfMusqQ"><i class="fa-brands fa-spotify"></i> spotify</a>',
+    confirmButtonText:'<a class="tex">Fechar</a>'
+
+  });
+}
+
+function barraInfo() {
+  Swal.fire({
+    title: 'Conheça-me',
+    html: '<a class="mos" href="https://vsco.co/jkvua"><i class="fa-solid fa-camera-retro"></i> vsco</a> <br> <a class="mos" href="https://www.linkedin.com/in/camila-ferreira-de-almeida-17b130272/"><i class="fa-brands fa-linkedin"></i> linkedin</a> <br> <a class="mos" href="https://intranet.ielsc.org.br/intranet/ferramentas/crmiel/sge/candidato/curriculo.pdf?candidato=2195763&vaga="><i class="fa-solid fa-newspaper"></i> currículo</a><br><a class="mos" href="https://open.spotify.com/user/6v1kv7fxuz60xhwevzou86lk8?si=95ZANTW4S3C4wZ1LfMusqQ"><i class="fa-brands fa-spotify"></i> spotify</a>',
+    confirmButtonText:'<a class="text">Fechar</a>'
   });
 }
