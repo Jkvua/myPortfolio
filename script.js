6246d2a092068e76986297e2a578be06dff1ae5d
Camila Ferreira <mila.e.ferreira28@gmail.com>
2024-05-16 14:11:55 -0300

    redirecionando os links

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function barraFerramentas() {
     title: "Conheça-me",
     //icon: 'info',
     text: "Caso queira saber mais sobre mim",
-    html: '<a class="rojo" href="https://vsco.co/jkvua"><i class="fa-solid fa-camera-retro"></i> vsco</a> <br> <a class="rojo" href="https://www.linkedin.com/in/camila-ferreira-de-almeida-17b130272/"><i class="fa-brands fa-linkedin"></i> linkedin</a> <br> <a class="rojo" href="https://intranet.ielsc.org.br/intranet/ferramentas/crmiel/sge/candidato/curriculo.pdf?candidato=2195763&vaga="><i class="fa-solid fa-newspaper"></i> currículo</a><br><a class="rojo" href="https://open.spotify.com/user/6v1kv7fxuz60xhwevzou86lk8?si=95ZANTW4S3C4wZ1LfMusqQ"><i class="fa-brands fa-spotify"></i> spotify</a>',
+    html: '<a class="rojo" target="_blank" href="https://vsco.co/jkvua"><i class="fa-solid fa-camera-retro"></i> vsco</a> <br> <a class="rojo" target="_blank" href="https://www.linkedin.com/in/camila-ferreira-de-almeida-17b130272/"><i class="fa-brands fa-linkedin"></i> linkedin</a> <br> <a class="rojo" target="_blank" href="https://drive.google.com/file/d/1LVTvYCjbkrGsHPRqtvQW852M7pd7aR5f/view?usp=sharing"><i class="fa-solid fa-newspaper"></i> currículo</a><br><a class="rojo" target="_blank" href="https://open.spotify.com/user/6v1kv7fxuz60xhwevzou86lk8?si=95ZANTW4S3C4wZ1LfMusqQ"><i class="fa-brands fa-spotify"></i> spotify</a>',
     confirmButtonText: "Fechar",
     cancelButtonArialLabel: "Thumbs down",
   });
@@ -14,7 +14,7 @@ function informacoesMe() {
   Swal.fire({
     title: 'Conheça-me',
     text: 'Caso queira saber mais de mim',
-    html: '<a class="rus" href="https://vsco.co/jkvua"><i class="fa-solid fa-camera-retro"></i> vsco</a> <br> <a class="rus" href="https://www.linkedin.com/in/camila-ferreira-de-almeida-17b130272/"><i class="fa-brands fa-linkedin"></i> linkedin</a> <br> <a class="rus" href="https://intranet.ielsc.org.br/intranet/ferramentas/crmiel/sge/candidato/curriculo.pdf?candidato=2195763&vaga="><i class="fa-solid fa-newspaper"></i> currículo</a><br><a class="rus" href="https://open.spotify.com/user/6v1kv7fxuz60xhwevzou86lk8?si=95ZANTW4S3C4wZ1LfMusqQ"><i class="fa-brands fa-spotify"></i> spotify</a>',
+    html: '<a class="rus" target="_blank" href="https://vsco.co/jkvua"><i class="fa-solid fa-camera-retro"></i> vsco</a> <br> <a class="rus" target="_blank" href="https://www.linkedin.com/in/camila-ferreira-de-almeida-17b130272/"><i class="fa-brands fa-linkedin"></i> linkedin</a> <br> <a class="rus" target="_blank" href="https://drive.google.com/file/d/1LVTvYCjbkrGsHPRqtvQW852M7pd7aR5f/view?usp=sharing"><i class="fa-solid fa-newspaper"></i> currículo</a><br><a class="rus" target="_blank" href="https://open.spotify.com/user/6v1kv7fxuz60xhwevzou86lk8?si=95ZANTW4S3C4wZ1LfMusqQ"><i class="fa-brands fa-spotify"></i> spotify</a>',
     confirmButtonText:'<a class="tex">Fechar</a>'
 
   });
@@ -23,7 +23,7 @@ function informacoesMe() {
 function barraInfo() {
   Swal.fire({
     title: 'Conheça-me',
-    html: '<a class="mos" href="https://vsco.co/jkvua"><i class="fa-solid fa-camera-retro"></i> vsco</a> <br> <a class="mos" href="https://www.linkedin.com/in/camila-ferreira-de-almeida-17b130272/"><i class="fa-brands fa-linkedin"></i> linkedin</a> <br> <a class="mos" href="https://intranet.ielsc.org.br/intranet/ferramentas/crmiel/sge/candidato/curriculo.pdf?candidato=2195763&vaga="><i class="fa-solid fa-newspaper"></i> currículo</a><br><a class="mos" href="https://open.spotify.com/user/6v1kv7fxuz60xhwevzou86lk8?si=95ZANTW4S3C4wZ1LfMusqQ"><i class="fa-brands fa-spotify"></i> spotify</a>',
+    html: '<a class="mos" target="_blank" href="https://vsco.co/jkvua"><i class="fa-solid fa-camera-retro"></i> vsco</a> <br> <a class="mos" target="_blank" href="https://www.linkedin.com/in/camila-ferreira-de-almeida-17b130272/"><i class="fa-brands fa-linkedin"></i> linkedin</a> <br> <a class="mos" target="_blank" href="https://drive.google.com/file/d/1LVTvYCjbkrGsHPRqtvQW852M7pd7aR5f/view?usp=sharing"><i class="fa-solid fa-newspaper"></i> currículo</a><br><a class="mos" target="_blank" href="https://open.spotify.com/user/6v1kv7fxuz60xhwevzou86lk8?si=95ZANTW4S3C4wZ1LfMusqQ"><i class="fa-brands fa-spotify"></i> spotify</a>',
     confirmButtonText:'<a class="text">Fechar</a>'
   });
 }
